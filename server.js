c17aef06b55f94d062187a72a341b0327257fc9e
= <jardriel.redes18@gmail.com>
2020-06-10 23:03:58 -0300

    ajuste

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const path = require('path');
 const nomeApp = process.env.fronEnd;
 const app = express();
- 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
- 
-app.get('/*', (req, res) => {
-res.sendFile(path.join(`/index.html`));
-});
+
+app.use(function(req, res, next) {
+    console.log("middleware 1")
+    next()
+  })
+  
+  app.get("/", function(req, res) {
+    console.log("requisição")
+    res.send("Requisição!")
+  })
  
 app.listen(process.env.PORT || 8081);
