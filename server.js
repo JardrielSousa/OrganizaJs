c6fb9a93bba0993dc3b03ba485b19def3b03025f
= <jardriel.redes18@gmail.com>
2020-06-11 20:57:18 -0300

    ajuste

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const nomeApp = process.env.fronEnd;
 const app = express();
  
-app.use(express.static(`${__dirname}/dist/frontEnd`));
+app.use(express.static(`${__dirname}/dist/fronEnd`));
  
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/frontEnd/index.html`));
+res.sendFile(path.join(`${__dirname}/dist/fronEnd/index.html`));
 });
  
 app.listen(process.env.PORT || 8081);
